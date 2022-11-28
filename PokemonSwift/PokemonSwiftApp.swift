669caa35c8c9b67a019318d5977de13bdc84adfb
Victor Tejeda <45402024+victortejeda@users.noreply.github.com>
2022-11-27 20:32:54 -0400

    Initial Commit

---
+++ PokemonSwift/PokemonSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonSwiftApp.swift
+//  PokemonSwift
+//
+//  Created by Victor Tejeda on 27/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PokemonSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
